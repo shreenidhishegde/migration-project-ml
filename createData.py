8aad1011091f852c4bfb43eca9f43afab0fd3782
shreenidhishegde <hegdekartik7@gmail.com>
2018-09-22 01:01:25 -0500

    Working copy of CSV writer

--- createData.py
+++ createData.py
@@ -1,39 +1,101 @@
 import csv
 
-# Import the data
-input_file = csv.DictReader(open("test.csv"))
-
-data = []
-status = []
-
-# Read the data
-for row in input_file:
-    # Read every row
-    for key in row:
-        # If the key has source URL
-        if('Source URL' in key):
-            data.append(row[key])
-        # If the key has Log
-        if('Log' in key):
-            stat = row[key]
-
-            if(stat == "Failed"):
-                status.append(0)
-            elif(stat == 'Success'):
-                status.append(1)
-            else:
-                print("Unknown Log type")
-
-url = []
-# Process the data
-for d in data:
-    url.append(d.split('/')[-1])
-
 special_chars = ['@', '&', '#']
-# Check for special characters
-for u in url:
-    for special_char in special_chars:
-        if(special_char in u):
-            print("Special Characters found")
-            print(u)
+
+def dataRead(path):
+    """
+     Reads CSV files
+    """
+    # Import the data
+    out_data = []
+    with open(path) as csvfile:
+        input_file = csv.DictReader(csvfile)
+        for row in input_file:
+            out_data.append(row)
+    return out_data
+
+def dataWrite(path, column_keys, out_data):
+    """
+        Writes CSV Files
+    """
+    with open(path, 'w') as f:  # Just use 'w' mode in 3.x
+        w = csv.DictWriter(f, column_keys)
+        w.writeheader()
+        for row in out_data:
+            w.writerow(row)
+
+    return None
+
+def readURL(out_data):
+    """
+        Extracts URLs
+    """
+    url = []
+    # Read the data
+    for row in out_data:
+        # Read every row
+        for key in row:
+            # If the key has source URL
+            if('Source URL' in key):
+                curr_url = row[key]
+                url.append(curr_url.split('/')[-1])
+    return url
+
+        # If the key has Log
+        # if('Log' in key):
+            # stat = row[key]
+
+            # if(stat == "Failed"):
+                # status.append(0)
+            # elif(stat == 'Success'):
+                # status.append(1)
+            # else:
+                # print("Unknown Log type")
+
+def findSpecialChars(url, special_chars):
+    is_special_char = []
+
+    # Check for special characters
+    for u in url:
+        flag = 0
+        for special_char in special_chars:
+            if(special_char in u):
+                flag = 1
+        is_special_char.append(flag)
+
+    return is_special_char
+
+def addNewRow(out_data, is_special_char):
+    for idx,row in enumerate(out_data):
+            row['Special Char'] = is_special_char[idx]
+
+    return out_data
+
+def main(readPath, writePath):
+
+    print("Reading CSV File\n")
+
+    # read CSV file
+    out_data = dataRead(readPath)
+
+    # Get the URLS
+    url = readURL(out_data)
+
+    # Find Special Chars
+    is_special_char = findSpecialChars(url, special_chars)
+
+    # Add new column
+    out_data = addNewRow(out_data, is_special_char)
+
+    # Get the headers
+    column_keys = out_data[0].keys()
+
+    print("Writing CSV Files\n")
+
+    # Write new CSV
+    dataWrite(writePath, column_keys, out_data)
+
+    print("Success!")
+
+main("test.csv", "test_11.csv")
 
